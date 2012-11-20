23f5f7667ae5832967956da59a1d02e1fe17a30f
mariusc <marius@ipworkshop.ro>
2012-11-21 00:33:35 +0100

    added city to the user

--- local-code/user.py
+++ local-code/user.py
@@ -1,9 +1,10 @@
 
 class User:
-    def __init__(self, id, username, coutry, jobs):
+    def __init__(self, id, username, coutry, city, jobs):
         self.id = id
         self.username = username
         self.coutry = coutry
+        self.city = city
         self.jobs = jobs            #user's skills
         
     def to_dict(self):

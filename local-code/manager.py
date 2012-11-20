4a34637ade6534f50df179aece95eaab4d4fb206
Ruxandra Nistor <roxanne0389@gmail.com>
2012-11-21 00:50:24 +0100

    modified manager

--- local-code/manager.py
+++ local-code/manager.py
@@ -1,5 +1,5 @@
-import FreelanceOAuthClient
-import Job
+import web.FreelanceOAuthClient as freelance_auth
+import job
 
 class Parser:
     def parseProjects(self, raw_data):
@@ -7,10 +7,12 @@ class Parser:
     def parseUsers(self, raw_data):
         print "TODO"
     def parseJobs(self, raw_data):
-        print "TODO"
+        print "TODO" + raw_data
    
-auth = FreelanceOAuthClient()  
+auth = freelance_auth.FreelanceOAuthClient()  
 parser = Parser()
+
+url = "Job/getJobList.json"
 resp = auth.send_request(url)
 jobs = parser.parseJobs(resp)
 

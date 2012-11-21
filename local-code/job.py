8f4107cfe3eb913db816e4ac522adb26056727a1
mariusc <marius@ipworkshop.ro>
2012-11-21 01:13:23 +0100

    updated fields in classes

--- local-code/job.py
+++ local-code/job.py
@@ -1,9 +1,10 @@
 
 class Job:
-    def __init__(self, id, name, projects_count):
+    def __init__(self, id, name, projects_count, seo_url):
         self.id = id
         self.name = name
         self.projects_count = projects_count
+        self.seo_url = seo_url
         
     def to_dict(self):
         out = {}
--- local-code/user.py
+++ local-code/user.py
@@ -1,18 +1,23 @@
 
 class User:
-    def __init__(self, id, username, coutry, city, jobs):
+    def __init__(self, id, username, coutry, city, jobs, rating, currency):
         self.id = id
         self.username = username
         self.coutry = coutry
         self.city = city
         self.jobs = jobs            #user's skills
+        self.rating = rating
+        self.currency = currency
         
     def to_dict(self):
         out = {}
         out["id"] = self.id
         out["username"] = self.username
         out["country"] = self.coutry
+        out["city"] = self.city
         out["jobs"] = self.jobs
+        out["rating"] = self.rating
+        out["currency"] = self.currency
         return out
     
 

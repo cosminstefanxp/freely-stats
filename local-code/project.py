505e71a0d21799c700e826947e1ec095a53f2fd7
mariusc <marius@ipworkshop.ro>
2012-11-21 15:40:49 +0100

    new constructor for project

--- local-code/project.py
+++ local-code/project.py
@@ -14,7 +14,22 @@ class Project:
         self.seller = seller
         self.bidders = bidders  #no idea how to get them
         self.accepted_bidder = acepted_bidder
+        
+    def __init__(self, id, name, start_date, jobs, bid_count, avg_bid):
+        self.id = id
+        self.name = name
+        self.start_date = start_date
+        self.jobs = jobs
+        self.bid_count = bid_count
+        self.avg_big = avg_bid
 
+        self.end_date = -1
+        self.buyer = -1
+        self.state = -1
+        self.shrt_descr = ''
+        self.seller = -1
+        self.bidders = []
+        self.accepted_bidder = -1
         
     def to_dict(self):
         out = {}

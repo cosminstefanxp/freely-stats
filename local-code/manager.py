37a29070f61a9d666ea607ff20990d2719a1f7dd
Ruxandra Nistor <roxanne0389@gmail.com>
2012-11-23 23:07:10 +0100

    organized code & working writing jobs

--- local-code/manager.py
+++ local-code/manager.py
@@ -24,40 +24,143 @@ class Parser:
             projects[id] = project.Project(id, name, startdate, jobtypecsv, bids, avg_bid)
         return projects
     
+    def parseProjectDetails(self, raw_data):
+        raw_json = json.loads(raw_data)
+        
     def parseUsers(self, raw_data):
         print "TODO"
-    def parseJobs(self, raw_data):
+        
+    def parseAllCategories(self, raw_data):
         raw_json = json.loads(raw_data)
-        raw_jobs = raw_json['json-result']['items']
+        raw_categories = raw_json['json-result']['items']["category"]
         jobs = []
-        for raw_job in raw_jobs:
-            id = raw_job['id']
-            name = raw_job['name']
-            count = raw_job['project_count']
-            seo_url = raw_job['seo_url']
-            jobs.append(job.Job(id, name, count, seo_url))
+        for raw_category in raw_categories:
+            cat_id = raw_category["id"]
+            print raw_category["id"],
+            print "    " + raw_category['name'] 
+            raw_jobs = raw_category["job"]
+            for raw_job in raw_jobs:  
+                print "\t",
+                print  raw_job['name'],
+                print "  ",
+                print raw_job['project_count']            
+                id = raw_job['id']
+                name = raw_job['name']
+                count = int(raw_job['project_count'])
+                seo_url = raw_job['seo_url']
+                jobs.append(job.Job(id, name, count, seo_url))
+            print "\n\n"
         return jobs
-   
+    
+    def parseMainCategories(self, raw_data):
+        raw_json = json.loads(raw_data)
+        raw_categories = raw_json['json-result']['items']["category"]
+        jobs = []
+        for raw_category in raw_categories:
+            cat_id = raw_category["id"]
+            print raw_category["id"],
+            print "    " + raw_category['name'] 
+            if cat_id == 1 or cat_id == 2 or cat_id == 4:
+                raw_jobs = raw_category["job"]
+                for raw_job in raw_jobs:  
+                    print "\t",
+                    print  raw_job['name'],
+                    print "  ",
+                    print raw_job['project_count']            
+                    id = raw_job['id']
+                    name = raw_job['name']
+                    count = int(raw_job['project_count'])
+                    seo_url = raw_job['seo_url']
+                    jobs.append(job.Job(id, name, count, seo_url))
+                    
+            print "\n\n"
+        return jobs
+
+class Manager:
+    def __init__(self):
+        self.auth = freelance_auth.FreelanceOAuthClient() 
+        self.parser = Parser()
+        self.foa = FileOA.Foa()
+        
+    def write_jobs(self):
+        url = "Job/getCategoryJobList.json"
+        resp = self.auth.send_request(url)
+        
+        jobs = self.parser.parseAllCategories(resp)
+        jobs.sort(key=lambda x: x.projects_count, reverse=True)
+        self.foa.writeJobsToFile(jobs, file_name="jobs_in_all_categories.json")
+        
+        
+        jobs = self.parser.parseMainCategories(resp)
+        jobs.sort(key=lambda x: x.projects_count, reverse=True)
+        self.foa.writeJobsToFile(jobs, file_name="jobs_in_main_categories.json")
+        
+        top_jobs = jobs[:100]
+        self.foa.writeJobsToFile(top_jobs, file_name="jobs_TOP100_in_main_categories.json")
+        
+    def write_jobs_for_main_categories(self):
+        url = "Job/getCategoryJobList.json"
+        resp = self.auth.send_request(url)
+        jobs = self.parser.parseMainCategories(resp)
+        jobs.sort(key=lambda x: x.projects_count, reverse=True)
+        top_jobs = jobs[:100]
+        #self.foa.writeJobsToFile(top_jobs, file_name="jobs_TOP100_in_main_categories.json")
+        
+        print "finished getting top jobs..."
+        top_job_names = [job.name for job in top_jobs]
+     #   print top_job_names
+        escaped_top_job_names = [urllib2.quote(job_name) for job_name in top_job_names]
+        joined = ",".join(escaped_top_job_names)
+        
+        print "Joined job names" + joined + "\n\n\n\n\n"
+        
+        url = "Project/searchProjects.json?searchjobtypecsv="+joined
+        resp = self.auth.send_request(url)
+        
+        print "\n"
+        print resp
+        projects = self.parser.parseProjects(resp)
+        #print projects
+
+manager = Manager()
+#manager.write_jobs()
+manager.write_jobs_for_main_categories()
+
+
+
+'''
 auth = freelance_auth.FreelanceOAuthClient()  
 parser = Parser()
-
-url = "Job/getJobList.json"
-resp = auth.send_request(url)
-print resp
-jobs = parser.parseJobs(resp)
-sorted_jobs = sorted(jobs, key=lambda x: x.projects_count, reverse=True)
-
 foa = FileOA.Foa()
-foa.writeJobsToFile(jobs)
 
-top_job_names = [job.name for job in sorted_jobs][:100]
+# 1 2 4
+#
+url = "Job/getCategoryJobList.json"
+resp = auth.send_request(url)
+
+jobs = parser.parseAllCategories(resp)
+jobs.sort(key=lambda x: x.projects_count, reverse=True)
+foa.writeJobsToFile(jobs, file_name="jobs_in_all_categories.json")
+
+
+jobs = parser.parseMainCategories(resp)
+jobs.sort(key=lambda x: x.projects_count, reverse=True)
+foa.writeJobsToFile(jobs, file_name="jobs_in_main_categories.json")
+
+top_jobs = jobs[:100]
+foa.writeJobsToFile(top_jobs, file_name="jobs_TOP100_in_main_categories.json")
+
+
+
+top_job_names = [job.name for job in top_jobs]
 print top_job_names
 escaped_top_job_names = [urllib2.quote(job_name) for job_name in top_job_names]
 joined = ",".join(escaped_top_job_names)
 
 print joined + "\n\n\n\n\n\n\n\n"
 
-url = "Project/searchProjects.json?searchjobtypecsv="#+joined
+url = "Project/searchProjects.json?searchjobtypecsv="+joined
 resp = auth.send_request(url)
 projects = parser.parseProjects(resp)
 print projects
+'''

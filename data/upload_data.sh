3846f828c877d6b5565a6e0b646b2cf6787685b4
Cosmin Stefan <cosminstefanxp@gmail.com>
2012-12-04 18:19:53 +0100

    Small updates

--- data/upload_data.sh
+++ data/upload_data.sh
@@ -9,7 +9,7 @@ fi
 case "$3" in 
 	"remote")
 	echo "Uploading data to remote server..."
-	 ~/.appengine_python/appcfg.py upload_data --config_file=bulkloader.yaml --filename="$2" --kind="$1" --url='http://freelystats.appspot.com/_ah/remote_api'
+	 ~/.appengine_python/appcfg.py --email=cosminstefanxp@gmail.com upload_data --config_file=bulkloader.yaml --filename="$2" --kind="$1" --url='http://freelystats.appspot.com/_ah/remote_api'
 	;;
 	*)
 	echo "Uploading data to local server..."
